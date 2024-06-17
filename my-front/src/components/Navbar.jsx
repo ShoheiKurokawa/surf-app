e9bfa5a7250fb1033db532195aeb3666575f555f
ShoheiKurokawa <shouhei.921@gmail.com>
2024-06-17 02:49:59 -1000

    ADD feature in Navbar

--- my-front/src/components/Navbar.jsx
+++ my-front/src/components/Navbar.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import {React, useState} from 'react'
+import '../styles/Navbar.css'
+
 
 const Navbar = () => {
-  return (
-    <nav>
-        <div>
-            <h1>Chill Surf</h1>
+
+    const [menuOpen, setMenuOpen] = useState(false);
+    const toggleMenu = () => {setMenuOpen(!menuOpen)}
+    const closeMenu = () => {setMenuOpen(false)}
+
+    return (
+        <nav>
             <div>
-                <a href="/">Home</a>
-                <a href="/about">About</a>
+                <h1>Chill Surf</h1>
+                <div>
+                    <a href="/">Home</a>
+                    <a href="/about">About</a>
+                    <a href="/dashboard">Dashboard</a>
+                </div>
+                <div className="menu" onClick={toggleMenu}>
+                    <image src="https://img.icons8.com/ios/50/000000/menu--v1.png" alt="menu"/>
+                </div>
+            
             </div>
-        </div>
-    </nav>
-  )
+
+        </nav>
+    )
 }
 
 export default Navbar
