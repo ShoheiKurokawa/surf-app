a1bede8efe19d8f3f2fbdae13ad0019fb9ff2c6a
ShoheiKurokawa <shouhei.921@gmail.com>
2024-06-16 21:04:21 -1000

    START front-end

---
+++ my-front/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <h1>Chill Surf</h1>
+            <div>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
